42272a3cbd5233e02efb0148a3d764f653e73ae6
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-08 22:57:46 +0100

    feat(accounts-serializer): Making the serializer for registration new user

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -9,3 +9,24 @@ class UserSerializer(serializers.ModelSerializer):
         fields = [
             "id", "first_name", "last_name", "email", "user_type", "avatar_url", "birth_date", "phone"
         ]
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "first_name", "last_name", "email", "password", "confirm_password", "user_type", "birth_date", "phone"
+        ]
+    
+    def validate(self, attrs):
+        if attrs["password"] != attrs["confirm_password"]:
+            raise serializers.ValidationError("Passwords don't match.")
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop("confirm_password")
+        user = User.objects.create_user(**validated_data)
+        return user
