4a20bbd48efe5b6b63918e4613315b7a050d7d6b
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-09 02:35:07 +0100

    feat(views-accounts): Working in the api views for register user

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import CustomTokenObtainPairSerializer
+from .serializers import CustomTokenObtainPairSerializer, UserRegistrationSerializer
 
 
 
@@ -8,3 +12,16 @@ User = get_user_model()
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def register(request):
+    serializer = UserRegistrationSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response(
+            {"message": "User created successfully."},
+            status=status.HTTP_201_CREATED
+        )
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
