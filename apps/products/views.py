df200a5a108dfda386623096d22827c8f3425be7
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-13 23:29:32 +0100

    feat(views): Create the apiv views for list the product

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from .models import Product
+from .serializers import ProductListSerializer
 
-# Create your views here.
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def products_list(request):
+    # Filter by store if provided
+    store_slug = request.query_params.get("store", None)
+    if store_slug:
+        products = Product.objects.filter(store__slug=store_slug, store__is_active=True)
+    else:
+        products = Product.objects.filter(feature=True, store__is_active=True)
+    
+    serializer = ProductListSerializer(products, many=True)
+    return Response(serializer.data)
