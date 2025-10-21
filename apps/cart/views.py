7f08beda7c10913cb4e12c7575b8dcaa7ed9192d
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-21 03:34:27 +0100

    feat(views): Create the api view for get the cart

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from .models import Cart
+from .serializers import CartSerializer
 
-# Create your views here.
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def get_cart(request, cart_code):
+    try:
+        cart = Cart.objects.get(cart_code=cart_code)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
+    except Cart.DoesNotExist:
+        return Response({"error": "Cart not found."}, status=status.HTTP_404_NOT_FOUND)
