0db91add73279806232c3fd7bb518949dd23e3dd
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-10-29 04:21:21 +0100

    feat(views): Create the api views for get the auth user cart

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -127,3 +127,14 @@ def create_user_cart(request):
         cart.save()
     serializer = CartSerializer(cart)
     return Response(serializer.data)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_user_cart(request):
+    try:
+        cart = Cart.objects.get(user=request.user)
+        serializer = CartSerializer(cart)
+        return Response(serializer.data)
+    except Cart.DoesNotExist:
+        return Response({"error": "Carrinho não encontrado."}, status=status.HTTP_404_NOT_FOUND)
