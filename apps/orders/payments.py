3198515b9806d034d314408ca27c1a24c8f1a809
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-02 00:51:22 +0100

    feat(payments): Create the methods for process payment.
    Simulate a gateway payment system that have 75% for everthing be ok.

--- apps/orders/payments.py
+++ apps/orders/payments.py
@@ -1,5 +1,6 @@
 import random
 import string
+from .models import Order, Payment
 
 
 # Simulação de sistema de pagamento para AOA-Kwanza
@@ -12,3 +13,52 @@ class AOAPaymentProcessor:
     def generate_reference():
         """Gera um número de referência para pagamento."""
         return f"REF-{"".join(random.choices(string.digits, k=12))}"
+    
+    @staticmethod
+    def process_payment(order: Order, payment_method, reference_number=None):
+        """
+        Processa o pagamento de um pedido
+
+        Args:
+            order: Objeto Order
+            payment_methdo: Método de pagamento ("reference", "mobile", "card")
+            reference_number: Número de referência (para pagamentos por referência)
+        
+        Returns:
+            tuple: (success: bool, transaction_id: str, message: str)
+        """
+
+        # Criar registro de pagamento
+        payment = Payment.objects.create(
+            order=order,
+            payment_method=payment_method,
+            amount=order.total_amount,
+            reference_number=reference_number
+        )
+
+        # Simulação de processamento de pagamento
+        # Em um ambiente real, aqui seria feita a integração com o gateway de pagamento
+        success = random.choice([True, True, True, False]) # 75% de chance de sucesso
+
+        if success:
+            # Gerar ID de transação
+            transaction_id = f"TXN-{"".join(random.choices(string.ascii_uppercase + string.digits, k=16))}"
+            payment.transaction_id = transaction_id
+            payment.payment_status = "completed"
+            payment.save()
+
+            # Atualizar stauts do pedido
+            order.payment_status = "paid"
+            order.status = "confirmed"
+            order.save()
+
+            return True, transaction_id, "Payment processed successfully"
+        else:
+            payment.payment_status = "failed"
+            payment.save()
+
+            # Atualizar status do pedido
+            order.payment_status = "failed"
+            order.save()
+
+            return False, None, "Pagamento falhou. Por favor, tenta de novo ou use um outro método."
