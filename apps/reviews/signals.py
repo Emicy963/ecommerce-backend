5dbde2f65120fab89c39a63e856f9a47de2ca43f
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-03 19:54:47 +0100

    feat(signal): Create the signal for update product.

---
+++ apps/reviews/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.db.models import Avg
+from .models import ProductRating, Review
+
+
+@receiver(post_save, sender=Review)
+def update_product_rating_on_save(sender, instance, **kwargs):
+    produt = instance.product
+    reviews = produt.reviews.all()
+    total_reviews = reviews.count()
+
+    reviews_average = reviews.aggregate(Avg("rating"))["rating__avg"] or 0.0
+
+    product_rating, created = ProductRating.objects.get_or_create(product=produt)
+    product_rating.average_rating = reviews_average
+    product_rating.total_reviews = total_reviews
+    product_rating.save()
