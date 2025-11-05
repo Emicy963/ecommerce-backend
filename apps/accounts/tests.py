af2b59e58f5f14f6cbce6f824c46c9e2ef589947
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-11-06 00:15:19 +0100

    test(accounts): Create the method for test the create normal user and super user.

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -17,3 +17,23 @@ class UserModelTests(TestCase):
             "last_name": "User",
             "user_type": "buyer"
         }
+    
+    def test_create_user(self):
+        """Testa a criação de um usuário comum"""
+        user = User.objects.create_user(**self.user_data)
+        self.assertEqual(user.username, self.user_data["username"])
+        self.assertEqual(user.email, self.user_data["email"])
+        self.assertEqual(user.user_type, self.user_data["user_type"])
+        self.assertTrue(user.check_password(self.user_data["password"]))
+    
+    def test_create_superuser(self):
+        """Testa a criação de um superusuário"""
+        admin_user = User.objects.create_superuser(
+            username="admin",
+            email="admin@example.com",
+            password="adminpass123"
+        )
+        self.assertEqual(admin_user.username, "admin")
+        self.assertEqual(admin_user.email, "admin@example.com")
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
